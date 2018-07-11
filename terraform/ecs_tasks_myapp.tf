6740a193b82e3503b3eb2cbd4bb0f8bb0451a375
Damien Touchette <damien@greenhouseinteractive.com>
2018-07-11 14:48:26 -0600

    check

--- terraform/ecs_tasks_myapp.tf
+++ terraform/ecs_tasks_myapp.tf
@@ -40,3 +40,26 @@ resource "aws_ecs_service" "myapp" {
     "aws_alb_listener.myapp"
   ]
 }
+
+resource "aws_autoscaling_group" "ecs-cluster" {
+  name = "${aws_ecs_cluster.fargate.name}"
+  min_size = 1
+  max_size = 4
+  adjustment_type = "ChangeInCapacity"
+  health_check_type = "EC2"
+  /* launch_configuration = "${aws_launch_configuration.ecs.name}" */
+  health_check_grace_period = "${var.health_check_grace_period}"
+
+  tag {
+    key = "Env"
+    value = "${var.environment_name}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key = "Name"
+    value = "${aws_ecs_cluster.fargate.name}"
+    propagate_at_launch = true
+  }
+}
+
