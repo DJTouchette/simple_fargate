50e7cb03ad6516e9158a22c8f1a6f5b0b873de0b
Damien Touchette <damien@greenhouseinteractive.com>
2018-07-12 09:59:18 -0600

    hello

--- docker/myapp/server.js
+++ docker/myapp/server.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello Fargate!');
 });
 
+app.post('/test', function (req, respons) => {
+  for(var i = 0; i < 20000) {
+    console.log(i);
+  }
+
+  res.send('counted to 20000');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
--- terraform/ecs_tasks_myapp.tf
+++ terraform/ecs_tasks_myapp.tf
@@ -41,25 +41,14 @@ resource "aws_ecs_service" "myapp" {
   ]
 }
 
-resource "aws_autoscaling_group" "ecs-cluster" {
-  name = "${aws_ecs_cluster.fargate.name}"
-  min_size = 1
-  max_size = 4
-  /* adjustment_type = "ChangeInCapacity" */
-  health_check_type = "EC2"
-  /* launch_configuration = "${aws_launch_configuration.ecs.name}" */
-  /* health_check_grace_period = "${var.health_check_grace_period}" */
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-example-"
+  image_id      = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
 
-  /* tag { */
-  /*   key = "Env" */
-  /*   value = "${var.environment_name}" */
-  /*   propagate_at_launch = true */
-  /* } */
-
-  /* tag { */
-  /*   key = "Name" */
-  /*   value = "${aws_ecs_cluster.fargate.name}" */
-  /*   propagate_at_launch = true */
-  /* } */
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
 
